db3156d2de6d5cdfb50aae3ef0ac81ff17a610ff
thisisthekap <christian.kapplmueller@tonestro.com>
2023-02-23 13:53:36 +0100

    fixed build.sh to use artifacts from carthage folder

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ cd "${0%/*}"
 
 ./carthage.sh update --use-xcframeworks 
 
+cd ios
+cp -r ../Carthage/Build/*.xcframework .
+cd ..
+
 xcodebuild archive -sdk iphoneos -project ios/FirebaseProxy.xcodeproj -scheme FirebaseProxy -configuration Release -archivePath Output/Output-iphoneos SKIP_INSTALL=NO
 xcodebuild archive -sdk iphonesimulator -project ios/FirebaseProxy.xcodeproj -scheme FirebaseProxy -configuration Release -archivePath Output/Output-iphonesimulator SKIP_INSTALL=NO
 
